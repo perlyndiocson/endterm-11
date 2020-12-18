9bf10c0cc3c581a25faeecd1c6885c0d913d7f3d
perlyndiocson <60122597+perlyndiocson@users.noreply.github.com>
2020-12-18 11:01:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,39 @@
-<html>
-<head>
-  <title> MERRY CHRISTMAS </title>
-  </head>
-  <body>
-    
-    <center><h1> MERRY CHRISTMAS EVERYBODY !! </h1></center>
-    
-    <div align="center" style="padding-top:350px;" class="fond">
-
-    <div class="contener_home_one">
-      <div class="fireplace">&nbsp;</div>
-      <div class="fireplace_top">&nbsp;</div>
-      <div class="triangle">&nbsp;</div>
-      <div class="parallelogram">&nbsp;</div>
-      <div class="door">&nbsp;</div>
-      <div class="window_one">&nbsp;</div>
-      <div class="window_two">&nbsp;</div>
-      <div class="home_base">&nbsp;</div>
-      <div class="christmas_tree"></div>
-      <div class="christmas_tree" style="left:-140px;"></div>
-      <div class="mountain_one"><div class="sub_mountain_one">&nbsp;</div></div>
-      <div class="mountain_two"><div class="sub_mountain_two">&nbsp;</div></div>
-      <div class="lutz">
-        <div class="lutin_pom">&nbsp;</div>
-        <div class="lutin_top">&nbsp;</div>
-        <div class="lutin_head">&nbsp;</div>
-        <div class="lutin_arm1">&nbsp;</div>
-        <div class="lutin_arm2">&nbsp;</div>
-        <div class="lutin_body">&nbsp;</div>
-        <div class="lutin_jb1">&nbsp;</div>
-        <div class="lutin_jb2">&nbsp;</div>
+<div class="main clearfix">
+  <div class="santa">
+    <div class="cap"></div>
+    <div class="face">
+      <div class="eyes">
+        <div class="left">
+          <span></span>
+        </div>
+        <div class="right">
+          <span></span>
+        </div>
+      </div>
+      <div class="mouth">
+        <div class="nose"></div>
       </div>
     </div>
-    <div class="contener_snow">
-      <div class="snowflakes">
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
-          <div class="snowflake">&nbsp;</div>
+    <div class="body">
+      <div class="left-hand"></div>
+      <div class="right-hand"></div>
+      <div class="belt">
+        <div class="buckle"></div>
+      </div>
+      <div class="legs">
       </div>
     </div>
-
-  <div style=" width:500px;height:9px; background-color:#ffffff; border-radius:5px;">&nbsp;</div>
-  
-  
-   <div style=" padding:5px; color:#639da8; font-weight:300; font-size:55px; font-family:'Roboto';padding-top:20px;">Merry <font style="font-weight:400;">Christmas</font></div>
-        <a href="http://texxsmith.com" style="text-decoration:none;" target="_blank"><div style="  color:#639da8; font-weight:300; font-size:20px; font-family:'Roboto';">From the Smith Family</div></a>
+  </div>
+  <div class="gift-holder">
+    <div class="gift">
+      <div class="ribbon"></div>
+    </div>
+    <div class="gift purple">
+      <div class="ribbon"></div>
+    </div>
+    <div class="gift orange">
+      <div class="ribbon"></div>
+    </div>
+  </div>
 </div>
+<p class="text">Merry Christmas</p>
