cec4a5bdb26db2845386deb3b1bda631d7ca3263
perlyndiocson <60122597+perlyndiocson@users.noreply.github.com>
2020-12-19 00:10:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,49 @@
-<html>
-<head>
-	
-<nav class="navbar-fixed-top navbar-default">
-<div class="container-fluid">
-  <div class="navbar-header">
-    <a class="navbar-brand" href="#">WELCOME TO KAI'S WEBSITE</a>
-  </div>
-  <div>
-    <ul class="nav navbar-nav navbar-right">
-      <li class="active"><a href="#intro">Home</a></li>
-      <li><a href="#svg">Svg</a></li>
-      <li><a href="#canvas">Canvas</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </div>
-</div>
-</nav><!-- /.navbar -->
-
-<!-- Section 1 -->
-<section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-      <h1 class="first-h1">Main Headline</h1>
-      <hr>
-        <p>During this time of faith and family, may the true meaning of Christmas fill you with joy. Wishing you a Merry Christmas and a blessed New Year. </p>
+   <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-    </div>
-  </div>
-</section>
-	</body>
-</html>
+    </nav>
+
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+      </div>
+
+    </div> <!-- /container -->
 
 
 <div class="window">
