54b6907f2e111f09c17cc66a933404c67d392a09
perlyndiocson <60122597+perlyndiocson@users.noreply.github.com>
2020-12-19 01:45:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -850,35 +850,35 @@ body {
 </style>
 
 
-  <!-- Site footer -->
+ <!-- Site footer -->
     <footer class="site-footer">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6">
-            <h6>It's about the community.</h6>
-            <p class="text-justify">Did you know everyone is a writer? Leave us a message to get started.</p>
+            <h6>About</h6>
+            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
           </div>
 
           <div class="col-xs-6 col-md-3">
-            <h6>Connect</h6>
+            <h6>Categories</h6>
             <ul class="footer-links">
-              <li><a href="https://www.coasterscorner.com/newsletter">Latest Issue</a></li>
-              
-              <li><a href="https://www.coasterscorner.com/contact">Contact Us</a></li>
-              <li><a href="https://www.coasterscorner.com/affiliates">Affiliates</a></li>
-             
-              
+              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
             </ul>
           </div>
 
           <div class="col-xs-6 col-md-3">
-            <h6>Learn</h6>
+            <h6>Quick Links</h6>
             <ul class="footer-links">
-              
-              <li><a href="https://www.coasterscorner.com">Newsletter</a></li>
-              <li><a href="https://podcasts.apple.com/ca/podcast/coasters-corner-podcast/id1433841752">Podcast</a></li>
-              <li><a href="https://www.coasterscorner.com/blog">Blog</a></li>
-              
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
             </ul>
           </div>
         </div>
@@ -887,16 +887,17 @@ body {
       <div class="container">
         <div class="row">
           <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
-         <a href="https://www.coasterscorner.com/">Coaster's Corner</a>.
+            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+         <a href="#">Scanfcode</a>.
             </p>
           </div>
 
           <div class="col-md-4 col-sm-6 col-xs-12">
             <ul class="social-icons">
-              
-              <li><a class="twitter" href="https://twitter.com/coasters_corner"><i class="fa fa-twitter"></i></a></li>
-            
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
             </ul>
           </div>
         </div>
